5898f71a3bbec0180f06c3e71f41e26f73b12f68
ydhwa <ydhwa_18@naver.com>
2019-06-03 12:57:18 +0900

    jenkins deploy: BootApp.java 수정

--- springboot-mysite/src/main/java/com/cafe24/mysite/BootApp.java
+++ springboot-mysite/src/main/java/com/cafe24/mysite/BootApp.java
@@ -2,12 +2,18 @@ package com.cafe24.mysite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootApp {
+public class BootApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApp.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BootApp.class);
+	}
 }
