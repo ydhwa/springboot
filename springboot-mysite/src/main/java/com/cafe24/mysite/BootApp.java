b045f7d080668ec8c1a444c878954f7de9190b02
ydhwa <ydhwa_18@naver.com>
2019-06-03 13:29:02 +0900

    web.xml 지움

--- springboot-mysite/src/main/java/com/cafe24/mysite/BootApp.java
+++ springboot-mysite/src/main/java/com/cafe24/mysite/BootApp.java
@@ -4,15 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//public class BootApp extends SpringBootServletInitializer {
 public class BootApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApp.class, args);
 	}
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return builder.sources(BootApp.class);
-//	}
 }
