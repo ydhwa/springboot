f33cafd2acc2bff4cf1775059dd69d3916c02762
ydhwa <ydhwa_18@naver.com>
2019-06-03 13:03:26 +0900

    일단 원래대로 돌림

--- springboot-mysite/src/main/java/com/cafe24/mysite/BootApp.java
+++ springboot-mysite/src/main/java/com/cafe24/mysite/BootApp.java
@@ -2,18 +2,17 @@ package com.cafe24.mysite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootApp extends SpringBootServletInitializer {
+//public class BootApp extends SpringBootServletInitializer {
+public class BootApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApp.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(BootApp.class);
-	}
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(BootApp.class);
+//	}
 }
